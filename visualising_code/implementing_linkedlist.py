df261d7d269cf8d74cda4e4b215d3871794d89e7
kamalabot <kamaljp@gmail.com>
2023-11-07 06:48:38 +0530

    Starting practice of the day

---
+++ visualising_code/implementing_linkedlist.py
@@ -0,0 +1,60 @@
+# Implementing Linked Lists 
+
+class Node:
+
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+
+    def __str__(self):
+        return self.val
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.head = None
+
+    def append(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            return
+        cur = self.head
+
+        while cur.next is not None:
+            cur = cur.next
+
+        cur.next = Node(val)
+
+    def print(self):
+        str = ""
+        cur = self.head
+        while cur is not None:
+            str += cur.val + "->"
+            cur = cur.next  # This is key step 
+ 
+        print(str)
+
+    def contains(self, val):
+        cur = self.head 
+        while cur is not None:
+            if cur.val == val:
+                return True
+
+            cur = cur.next
+        
+        return False
+
+
+ll1 = LinkedList()
+
+ll1.append('a')
+ll1.append('b')
+ll1.append('c')
+ll1.append('d') # new tail
+
+ll1.print()
+
+print(ll1.contains('x'))
+print(ll1.contains('y'))
+print(ll1.contains('a'))
+print(ll1.contains('c'))
