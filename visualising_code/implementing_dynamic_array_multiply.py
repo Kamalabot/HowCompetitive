93327d340255be4260e72baecd753f5e0c569059
kamalabot <kamaljp@gmail.com>
2023-11-14 07:32:25 +0530

    array_multiplier progressing

---
+++ visualising_code/implementing_dynamic_array_multiply.py
@@ -0,0 +1,33 @@
+# Implementing the multiplication of the dynamic array of elements 
+# Refer to problem 1 in Coderbyte_visualising_dynamic_array.dio"
+from typing import List
+
+
+# def array_multiplier(numlist: List[int]) -> List[int]:
+#     """Brute force Solution"""
+#     # Create a result array to hold the result
+#     result = []
+#     # loop over ind,num of numlist 
+#     for ind, ele in enumerate(numlist):
+#         # create temp variable to hold product
+#         pdt = 1
+#         # For each ind, loop over rest of elements
+#         for loc, num in enumerate(numlist):
+#             # only if loc != ind
+#             if loc != ind:
+#             # multiply them and store result in product
+#                 pdt *= num
+#         # append product to result
+#         result.append(pdt)
+#     # return result
+#     return result
+
+# The above solution is O(n^2) as its nested loops
+
+def array_multiplier(numlist: List[int]) -> List[int]:
+    """Greedy Algorithm"""
+    
+
+print(array_multiplier([5, 8, 6, 9]))  # [432, 270, 360, 240]
+print(array_multiplier([1, 2, 3, 4]))  # [24, 12, 8, 6]
+print(array_multiplier([1, 2, 3, 4, 6, 8, 7, 21, 32, 87, 97, 66, 57, 23, 42, 79]))  # [13129064682645897216, 6564532341322948608, 4376354894215299072, 3282266170661474304, 2188177447107649536, 1641133085330737152, 1875580668949413888, 625193556316471296, 410283271332684288, 150908789455699968, 135351182295318528, 198925222464331776, 230334468116594688, 570828899245473792, 312596778158235648, 166190692185391104]
