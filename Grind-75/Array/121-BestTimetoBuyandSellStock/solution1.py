c9ad8ab49db05e3c049f5e6c327b8dbf59130011
kamalabot <kamaljp@gmail.com>
2024-02-20 21:49:31 +0530

    commented stock price soln

--- Grind-75/Array/121-BestTimetoBuyandSellStock/solution1.py
+++ Grind-75/Array/121-BestTimetoBuyandSellStock/solution1.py
@@ -3,13 +3,17 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxGap = 0
-        left, right = 0, 1
-        while right < len(prices):
+        maxGap = 0  # initialize maxgap variable
+        left, right = 0, 1  # initialize left and right 0, 1
+        while right < len(prices):  # while the right is less than list
             if prices[left] < prices[right]:
+                # check if price @ left < price @ right
                 maxGap = max(maxGap, prices[right] - prices[left])
-                right += 1
+                #  get the max of maxGap & diff of right - left
+                right += 1  # increment the right by one
             else:
+                # price @ left > price @ right
                 left, right = right, right + 1
+                # shift to the next pair
 
         return maxGap
