6efadfefa0da18293483c03063d179b1c166bf83
Kamalabot <35370462+Kamalabot@users.noreply.github.com>
2024-09-25 05:56:54 +0530

    intuition of knapsack_dp_15.py

--- patterns_prac/bt_dp_prac0/knapsack_dp_15.py
+++ patterns_prac/bt_dp_prac0/knapsack_dp_15.py
@@ -4,14 +4,22 @@
 
 def knapsack(weights, values, W):
     n = len(weights)
+# creating a 2d array, where the row is W+1 length and col is n+1 length 
     dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+# enumerate each row 
     for i in range(1, n + 1):
+# enumerate each weight val in the row
+# as the weights are increasing by 1
         for w in range(1, W + 1):
+# chk if current weight is can match the weight value
             if weights[i - 1] <= w:
+# below the dp array is filled by taking the
+# the max as shown below
                 dp[i][w] = max(
                     dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]
                 )
             else:
+# else use the value in earlier row's column
                 dp[i][w] = dp[i - 1][w]
     return dp[n][W]
 
